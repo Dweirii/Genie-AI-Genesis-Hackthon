081d46327aad3d1dca00ffde6a24a4bda5b7b471
Dweirii <zaiddweiri962@gmail.com>
2025-11-15 19:15:13 +0300

    feat: integrate Jotai for state management and enhance Providers component

---
+++ apps/widget/modules/widget/constants.ts
@@ -0,0 +1,12 @@
+export const WIDGET_SCREENS = [
+    "error",
+    "loading",
+    "voice",
+    "auth",
+    "selection",
+    "chat",
+    "inbox",
+    "contact",
+] as const;
+
+export const CONTACT_SESSION_KEY = "genie_contact_session";
--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,17 +1,32 @@
 "use client";
 
+import { useAtomValue } from "jotai";
 import { WidgetFooter } from "../components/widget-footer";
 import { WidgetHeader } from "../components/widget-header";
 import { WidgetAuthScreen } from "../screens/widget-auth-screen";
+import { screenAtom } from "../../atoms/widget-atoms";
 
 interface Props {
     organizationId: string;
 };
 
 export const WidgetView = ({ organizationId } : Props) => {
+    const screen = useAtomValue(screenAtom);
+
+    const screenComponents = {
+        auth: <WidgetAuthScreen/>,
+        loading: <div>Loading...</div>,
+        error: <div>Error occurred.</div>,
+        voice: <div>Voice Screen</div>,
+        selection: <div>Selection Screen</div>,
+        chat: <div>Chat Screen</div>,
+        inbox: <div>Inbox Screen</div>,
+        contact: <div>Contact Screen</div>,
+    }
+
     return (
         <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-            <WidgetAuthScreen/>
+            {screenComponents[screen]}
         </main>
-    )
-}
+    );
+};
